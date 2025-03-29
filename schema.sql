b8cec58af48ef9707eff5b21ad202706a973762c
Matilainen Tuuli K <matuuli@cubbli24-gold.cs.helsinki.fi>
2025-03-29 19:06:49 +0200

    Add new item form

--- schema.sql
+++ schema.sql
@@ -3,3 +3,12 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE items (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    book_name TEXT,
+    stars INT,
+    review TEXT,
+    user_id INTEGER REFERENCES users
+);
